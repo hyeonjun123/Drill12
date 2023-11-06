af7d54e0b65aaa005974774c00ba7651fa5871af
Im Hyeon Jun <hyeonjun3519@naver.com>
2023-11-06 10:53:51 +0900

    Initial commit

---
+++ game_world.py
@@ -0,0 +1,44 @@
+objects = [[] for _ in range(4)]
+
+# fill here
+
+def add_object(o, depth = 0):
+    objects[depth].append(o)
+
+def add_objects(ol, depth = 0):
+    objects[depth] += ol
+
+
+def update():
+    for layer in objects:
+        for o in layer:
+            o.update()
+
+
+def render():
+    for layer in objects:
+        for o in layer:
+            o.draw()
+
+# fill here
+
+
+def remove_object(o):
+    for layer in objects:
+        if o in layer:
+            layer.remove(o)
+            return
+    raise ValueError('Cannot delete non existing object')
+
+
+def clear():
+    for layer in objects:
+        layer.clear()
+
+
+
+# fill here
+def collide(a,b):
+    left_a, bottom_a, right_a, top_a = a.get_bb()
+    
+
